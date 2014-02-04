a336c0c3ebb1eaf5acc7cc4a33f7c8a5bda83cd2
Elisabeth Waltz <elisabeth.waltz@gmail.com>
2014-02-04 11:01:11 -0500

    TDDd Location.box_coordinates

--- app/models/location.rb
+++ app/models/location.rb
@@ -15,4 +15,16 @@
 
 class Location < ActiveRecord::Base
   has_many :projects
+
+  def self.box_coordinates(latitude, longitude, distance=0.3)
+    # Return hash of coordinates
+    # Outside can be accessed by loc = Location(10, 10)
+    #                            loc[:max_y]
+    {
+      max_x: latitude + distance,
+      max_y: longitude + distance,
+      min_x: latitude - distance,
+      min_y: longitude - distance
+    }
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -6,4 +6,16 @@ describe Location do
     subject { location }
     it { should have_many(:projects) }
   end
+
+  describe '.box_coordinates' do
+    it 'should return a box of nearby coordinates' do
+      Location.box_coordinates(40, 20).should eq({max_x: 40.3, max_y: 20.3, min_x: 39.7, min_y: 19.7})
+      Location.box_coordinates(10, -5).should eq({max_x: 10.3, max_y: -4.7, min_x: 9.7, min_y: -5.3})
+    end
+
+    it 'should optionally take a distance and return a valid box' do
+      Location.box_coordinates(40, 20, 0.1).should eq({max_x: 40.1, max_y: 20.1, min_x: 39.9, min_y: 19.9})
+      Location.box_coordinates(10, -5, 1).should eq({max_x: 11, max_y: -4, min_x: 9, min_y: -6})
+    end
+  end
 end
